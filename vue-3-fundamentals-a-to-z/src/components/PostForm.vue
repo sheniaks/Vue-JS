73134decd3bb3fa86b44ad6d1a93f8693a9a4a5c
sheniaks <sheniaks@gmail.com>
2022-02-20 18:35:15 +0200

    vue 3 fundamentals app with add \ sort\ search \ pagination

---
+++ vue-3-fundamentals-a-to-z/src/components/PostForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <my-input v-model="post.title" type="text" placeholder="Название" />
+    <my-input v-model="post.body" type="text" placeholder="Описание" />
+    <my-button class="btn" @click="createPost" style="margin-top: 15px"
+      >Создать</my-button
+    >
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit("create", this.post, "second param", "third");
+      this.post = {
+        title: "",
+        body: "",
+      };
+    },
+  },
+  // watch: {
+  //   post: {
+  //     handler(newVal) {
+  //       console.log(newVal);
+  //     },
+  //     deep:true
+  //   },
+  // },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ vue-3-fundamentals-a-to-z/src/components/UI/MyInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <input
+    :value="modelValue"
+    @input="updateInput"
+    class="input"
+    type="text"
+    name=""
+    id=""
+  />
+</template>
+
+<script>
+export default {
+  name: "my-input",
+  props: {
+    modelValue: [String, Number],
+  },
+  methods: {
+    updateInput(event) {
+      this.$emit("update:modelValue", event.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  margin-top: 15px;
+  padding: 10px 15px;
+}
+</style>
