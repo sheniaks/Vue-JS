5b3bc59b9f9ce2ec1121af2a19fc000448ff4a8f
sheniaks <sheniaks@gmail.com>
2022-02-20 23:50:43 +0200

    vue router static navigation

---
+++ vue-3-fundamentals-a-to-z/src/pages/Main.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+       <h1>Добро пожаловать !!!</h1>
+       <router-link to="/posts">Посты</router-link> <br>
+       <router-link to="/about">Информация о сайте</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
