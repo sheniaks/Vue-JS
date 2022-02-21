10aca2f29a1164b36a49866052d18d8ce682d983
sheniaks <sheniaks@gmail.com>
2022-02-21 15:57:21 +0200

    dynamic navigation with router

--- vue-3-fundamentals-a-to-z/src/router/router.js
+++ vue-3-fundamentals-a-to-z/src/router/router.js
@@ -1,5 +1,6 @@
 import Main from "@/pages/Main";
 import PostPage from "@/pages/PostPage";
+import PostIdPage from "@/pages/PostIdPage";
 import About from "@/pages/About";
 
 import { createRouter, createWebHistory } from "vue-router";
@@ -13,10 +14,14 @@ const routes = [
     {
         path: '/posts',
         component: PostPage
-    },
+    }, 
     {
         path: '/about',
         component: About
+    },
+    {
+        path: '/posts/:id',
+        component: PostIdPage
     }
 ]
 
