93ccbecf6b1b9a0a6881fb2ec8302ab3486b8ad1
sheniaks <sheniaks@gmail.com>
2022-02-21 16:59:15 +0200

    custom directives: v-intersection \ v-focus

--- vue-3-fundamentals-a-to-z/src/main.js
+++ vue-3-fundamentals-a-to-z/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import App from "./App";
 import components from "@/components/UI";
-import router from "./router/router";
+import router from "@/router/router";
+import VIntersection from "@/directives/VIntersection";
+import directives from "@/directives";
 
 const app = createApp(App);
 
@@ -9,7 +11,10 @@ components.forEach((component) => {
   app.component(component.name, component);
 });
 
+directives.forEach((directive) => {
+  app.directive(directive.name, directive);
+});
 
-app
-.use(router)
-.mount("#app");
+app.directive("intersection", VIntersection);
+
+app.use(router).mount("#app");
